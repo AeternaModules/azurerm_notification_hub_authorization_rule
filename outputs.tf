af2f8d96943c54d07caa5141334e2f30abc6b9b6
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,54 @@
 output "notification_hub_authorization_rules" {
   description = "All notification_hub_authorization_rule resources"
   value       = azurerm_notification_hub_authorization_rule.notification_hub_authorization_rules
+  sensitive   = true
+}
+output "notification_hub_authorization_rules_listen" {
+  description = "List of listen values across all notification_hub_authorization_rules"
+  value       = [for k, v in azurerm_notification_hub_authorization_rule.notification_hub_authorization_rules : v.listen]
+}
+output "notification_hub_authorization_rules_manage" {
+  description = "List of manage values across all notification_hub_authorization_rules"
+  value       = [for k, v in azurerm_notification_hub_authorization_rule.notification_hub_authorization_rules : v.manage]
+}
+output "notification_hub_authorization_rules_name" {
+  description = "List of name values across all notification_hub_authorization_rules"
+  value       = [for k, v in azurerm_notification_hub_authorization_rule.notification_hub_authorization_rules : v.name]
+}
+output "notification_hub_authorization_rules_namespace_name" {
+  description = "List of namespace_name values across all notification_hub_authorization_rules"
+  value       = [for k, v in azurerm_notification_hub_authorization_rule.notification_hub_authorization_rules : v.namespace_name]
+}
+output "notification_hub_authorization_rules_notification_hub_name" {
+  description = "List of notification_hub_name values across all notification_hub_authorization_rules"
+  value       = [for k, v in azurerm_notification_hub_authorization_rule.notification_hub_authorization_rules : v.notification_hub_name]
+}
+output "notification_hub_authorization_rules_primary_access_key" {
+  description = "List of primary_access_key values across all notification_hub_authorization_rules"
+  value       = [for k, v in azurerm_notification_hub_authorization_rule.notification_hub_authorization_rules : v.primary_access_key]
+  sensitive   = true
+}
+output "notification_hub_authorization_rules_primary_connection_string" {
+  description = "List of primary_connection_string values across all notification_hub_authorization_rules"
+  value       = [for k, v in azurerm_notification_hub_authorization_rule.notification_hub_authorization_rules : v.primary_connection_string]
+  sensitive   = true
+}
+output "notification_hub_authorization_rules_resource_group_name" {
+  description = "List of resource_group_name values across all notification_hub_authorization_rules"
+  value       = [for k, v in azurerm_notification_hub_authorization_rule.notification_hub_authorization_rules : v.resource_group_name]
+}
+output "notification_hub_authorization_rules_secondary_access_key" {
+  description = "List of secondary_access_key values across all notification_hub_authorization_rules"
+  value       = [for k, v in azurerm_notification_hub_authorization_rule.notification_hub_authorization_rules : v.secondary_access_key]
+  sensitive   = true
+}
+output "notification_hub_authorization_rules_secondary_connection_string" {
+  description = "List of secondary_connection_string values across all notification_hub_authorization_rules"
+  value       = [for k, v in azurerm_notification_hub_authorization_rule.notification_hub_authorization_rules : v.secondary_connection_string]
+  sensitive   = true
+}
+output "notification_hub_authorization_rules_send" {
+  description = "List of send values across all notification_hub_authorization_rules"
+  value       = [for k, v in azurerm_notification_hub_authorization_rule.notification_hub_authorization_rules : v.send]
 }
 
